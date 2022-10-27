e08a6be7e1f7c13d16867b752f6098cfd92ca6b1
Deividas Juozapavicius <juozapaviciusd@gmail.com>
2022-10-27 15:29:38 +0300

    task 8 finished

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calc(num1, num2) {
+  this.num1 = num1;
+  this.num2 = num2;
+}
+
+Calc.prototype.sum = function () {
+  return this.num1 + this.num2;
+};
+Calc.prototype.subtraction = function () {
+  return this.num1 - this.num2;
+};
+Calc.prototype.multiplication = function () {
+  return this.num1 * this.num2;
+};
+Calc.prototype.division = function () {
+  return this.num1 / this.num2;
+};
+
+const calculated = new Calc(420, 69);
+console.log(calculated.sum());
+console.log(calculated.subtraction());
+console.log(calculated.multiplication());
+console.log(calculated.division());
